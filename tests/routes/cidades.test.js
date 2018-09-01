147ba461ebdd8737d9ff4ed993644d5722fc0e17
João Marcus de Lemos Fernandes <jmarcusfernandes@gmail.com>
2018-09-01 11:55:09 -0300

    feat: changed header name

--- tests/routes/cidades.test.js
+++ tests/routes/cidades.test.js
@@ -10,48 +10,48 @@ describe('Test Routes', () => {
     const Manager = new TokenManager(configs);
     const token = Manager.createToken();
 
-    describe('GET /cidade', () => {
+    describe('GET /cidades', () => {
       test('should have status 401 without header', () => {
         return request(app)
-          .get('/v1/cidade')
+          .get('/v1/cidades')
           .expect(401);
       });
 
       test('should have status 200 with header', () => {
         return request(app)
-          .get('/v1/cidade')
-          .set('x-api-key', token)
+          .get('/v1/cidades')
+          .set('X-Api-Key', token)
           .expect(200);
       });
 
       test('should have body with array', () => {
         return request(app)
-          .get('/v1/cidade')
-          .set('x-api-key', token)
+          .get('/v1/cidades')
+          .set('X-Api-Key', token)
           .then(response => {
             expect(typeof response.body).toBe('object');
           });
       });
     });
 
-    describe('POST /cidade', () => {
+    describe('POST /cidades', () => {
       test('should have status 401 without header', () => {
         return request(app)
-          .post('/v1/cidade')
+          .post('/v1/cidades')
           .expect(401);
       });
 
       test('should have status 400 without body', () => {
         return request(app)
-          .post('/v1/cidade')
-          .set('x-api-key', token)
+          .post('/v1/cidades')
+          .set('X-Api-Key', token)
           .expect(400);
       });
 
       test('should have status 200 with body', () => {
         return request(app)
-          .post('/v1/cidade')
-          .set('x-api-key', token)
+          .post('/v1/cidades')
+          .set('X-Api-Key', token)
           .send({ nome: 'Lambari', estadoId: '1' })
           .expect(200)
           .then(response => {
@@ -61,41 +61,41 @@ describe('Test Routes', () => {
       });
     });
 
-    describe('GET /cidade/:cidadeId', () => {
+    describe('GET /cidades/:cidadeId', () => {
       test('should have status 401 without header', () => {
         return request(app)
-          .get('/v1/cidade/1')
+          .get('/v1/cidades/1')
           .expect(401);
       });
 
       test('should have status 200 with header', () => {
         return request(app)
-          .get('/v1/cidade/1')
-          .set('x-api-key', token)
+          .get('/v1/cidades/1')
+          .set('X-Api-Key', token)
           .expect(200);
       });
 
       test('should have body with array', () => {
         return request(app)
-          .get('/v1/cidade/1')
-          .set('x-api-key', token)
+          .get('/v1/cidades/1')
+          .set('X-Api-Key', token)
           .then(response => {
             expect(typeof response.body).toBe('object');
           });
       });
     });
 
-    describe('PUT /cidade/:cidadeId', () => {
+    describe('PUT /cidades/:cidadeId', () => {
       test('should have status 401 without header', () => {
         return request(app)
-          .put('/v1/cidade/1')
+          .put('/v1/cidades/1')
           .expect(401);
       });
 
       test('should update entity', () => {
         return request(app)
-          .put('/v1/cidade/1')
-          .set('x-api-key', token)
+          .put('/v1/cidades/1')
+          .set('X-Api-Key', token)
           .send({ nome: 'Lambari' })
           .expect(200)
           .then(response => {
@@ -105,17 +105,17 @@ describe('Test Routes', () => {
       });
     });
 
-    describe('DELETE /cidade/:cidadeId', () => {
+    describe('DELETE /cidades/:cidadeId', () => {
       test('should have status 401 without header', () => {
         return request(app)
-          .delete('/v1/cidade/1')
+          .delete('/v1/cidades/1')
           .expect(401);
       });
 
       test('should update entity', () => {
         return request(app)
-          .delete('/v1/cidade/1')
-          .set('x-api-key', token)
+          .delete('/v1/cidades/1')
+          .set('X-Api-Key', token)
           .expect(200)
           .then(response => {
             expect(response.body.length).toBe(2);
@@ -123,25 +123,25 @@ describe('Test Routes', () => {
       });
     });
 
-    describe('GET /cidade/search', () => {
+    describe('GET /cidades/search', () => {
       test('should have status 401 without header', () => {
         return request(app)
-          .get('/v1/cidade/search')
+          .get('/v1/cidades/search')
           .expect(401);
       });
 
       test('should have status 200 with header', () => {
         return request(app)
-          .get('/v1/cidade/search')
-          .set('x-api-key', token)
+          .get('/v1/cidades/search')
+          .set('X-Api-Key', token)
           .query({ content: 'false' })
           .expect(200);
       });
 
       test('should have body with array', () => {
         return request(app)
-          .get('/v1/cidade/search')
-          .set('x-api-key', token)
+          .get('/v1/cidades/search')
+          .set('X-Api-Key', token)
           .query({ content: 'false' })
           .then(response => {
             expect(typeof response.body).toBe('object');
@@ -153,8 +153,8 @@ describe('Test Routes', () => {
 
       test('should have the selected aggregations', () => {
         return request(app)
-          .get('/v1/cidade/search')
-          .set('x-api-key', token)
+          .get('/v1/cidades/search')
+          .set('X-Api-Key', token)
           .query({ content: 'false', filter: [JSON.stringify({ type: '_id', value: 1 })] })
           .then(response => {
             expect(response.body.selectedFilters).toEqual([{ type: '_id', value: 1 }]);
